5b86b403ae9ef2669db1a4c967c69bacfa14c06b
L4xus⚡️ <cool5tarXV@gmail.com>
2020-11-10 16:38:18 +0800

    Create uninstall.sh

---
+++ install-scripts/official/termux-chess/uninstall.sh
@@ -0,0 +1,5 @@
+rm -rf /data/data/com.termux/files/rootstrap/programs/termux-chess/
+
+rm -rf /data/data/com.termux/files/rootstrap/install-scripts/termux-chess/
+
+rm -rf /data/data/com.termux/files/rootstrap/uninstall-scripts/termux-chess/
